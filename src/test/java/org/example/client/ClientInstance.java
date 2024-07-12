d53a298371dc3b0183e1ba7ac6257f81640f8c4a
ASJ234 <143750038+ASJ234@users.noreply.github.com>
2024-07-12 09:10:33 +0300

    added comments to make code easily understood

--- src/test/java/org/example/client/ClientInstance.java
+++ src/test/java/org/example/client/ClientInstance.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class ClientInstance {
-    // define attributes for the ClientInstance object
+    // Define attributes for the ClientInstance object
     String hostname;
     int port;
     String clientId;
@@ -18,13 +18,14 @@ public class ClientInstance {
     boolean isStudent;
     boolean isAuthenticated;
 
+    // Constructor to initialize ClientInstance with hostname, port, and user
     public ClientInstance(String hostname, int port, User user) {
-        // constructor class for the client instance
         this.hostname = hostname;
         this.port = port;
         this.user = user;
     }
 
+    // Method to validate if the input string is a valid JSON object
     public static boolean isValid(String input) {
         String regex = "^\\{.*\\}$";
         Pattern pattern = Pattern.compile(regex, Pattern.DOTALL);
@@ -32,25 +33,33 @@ public class ClientInstance {
         return pattern.matcher(input).matches();
     }
 
+    // Method to display a set of questions from a challenge object and collect answers from the user
     public JSONArray displayQuestionSet(JSONObject challengeObj) {
+        // Print challenge details
         System.out.println("CHALLENGE " + challengeObj.getInt("challenge_id") + " (" + challengeObj.get("challenge_name") + ")");
         Scanner scanner = new Scanner(System.in);
 
+        // Get questions from challenge object and initialize solutions array
         JSONArray questions = challengeObj.getJSONArray("questions");
         JSONArray solutions = new JSONArray();
-        this.cache = 0;
+        this.cache = 0; // Reset cache
         int count = 1;
+
+        // Iterate through each question
         for (int i = 0; i < questions.length(); i++) {
             JSONObject question = questions.getJSONObject(i);
             JSONObject answer = new JSONObject();
-            this.cache += (byte) question.getInt("score");
+            this.cache += (byte) question.getInt("score"); // Add question score to cache
 
+            // Display question and score
             System.out.println(count + ". " + question.get("question") + " (" + question.get("score") + " Marks)");
 
+            // Collect answer from user
             answer.put("question_id", question.getInt("id"));
             System.out.print(" - ");
             answer.put("answer", scanner.nextLine());
 
+            // Add answer to solutions array
             solutions.put(answer);
             count++;
             System.out.print("\n");
@@ -58,32 +67,30 @@ public class ClientInstance {
         return solutions;
     }
 
+    // Method to start the client instance and interact with the server
     public void start() throws IOException {
-        // Todo: create a parent menu
-
-        // execute code for interacting with the server
+        // Execute code for interacting with the server
         try (
                 Socket socket = new Socket(hostname, port);
                 BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
                 BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
         ) {
+            // Initialize client ID and serializer
             this.clientId = (String) socket.getInetAddress().getHostAddress();
             Serializer serializer = new Serializer(this.user);
 
+            // Prompt user for a command
             System.out.print("[Enter the command] (" + this.user.username + "): ");
-            // read command line input
 
             // Continuously read from the console and send to the server
             ClientController clientController = new ClientController(user);
             String regex = "^\\{.*\\}$";
             Pattern pattern = Pattern.compile(regex);
 
-
             String userInput;
             while ((userInput = consoleInput.readLine()) != null) {
-                // send command to the server
-
+                // Handle logout command
                 if (userInput.equals("logout") && (this.user.isAuthenticated)) {
                     System.out.println("Session successfully logged out");
                     this.user.logout();
@@ -91,22 +98,29 @@ public class ClientInstance {
                     continue;
                 }
 
+                // Serialize the user input command
                 String serializedCommand = serializer.serialize(userInput);
 
+                // Check if the serialized command is valid
                 if (isValid(serializedCommand)) {
+                    // Send the command to the server
                     output.println(serializedCommand);
 
-                    // read response here from the server
+                    // Read response from the server
                     String response = input.readLine();
 
+                    // Execute the response using the client controller
                     this.user = clientController.exec(response);
 
+                    // Check if the user's output is a valid JSON object
                     if (!pattern.matcher(this.user.output).matches()) {
                         System.out.println("\n" + user.output + "\n");
                     } else {
+                        // Parse questions from the user's output and collect answers
                         JSONObject questions = new JSONObject(this.user.output);
                         JSONArray answerSet = displayQuestionSet(questions);
 
+                        // Create an object to send the answers back to the server
                         JSONObject obj = new JSONObject();
                         obj.put("attempt", answerSet);
                         obj.put("participant_id", this.user.id);
@@ -114,18 +128,21 @@ public class ClientInstance {
                         obj.put("challenge_id", questions.getInt("challenge_id"));
                         obj.put("total_score", this.cache);
 
+                        // Send the answers to the server
                         String inp = obj.toString();
                         output.println(inp);
                     }
                 } else {
+                    // Print invalid serialized command
                     System.out.println(serializedCommand);
                 }
-                // prompt for the next instruction
+                // Prompt for the next instruction
                 System.out.print("[Enter the command] (" + this.user.username + "): ");
             }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
+            // Print connection timeout message
             System.out.println("Connection with the server timeout");
         }
     }
